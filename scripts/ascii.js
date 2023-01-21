54b6d0650fa13d0b4a1fdc78e6984098ea228fc9
Hang <hangmt03bk@yahoo.com>
2023-01-21 00:27:31 -0600

    [Hang] 01/21/2023 assignment5: animation

---
+++ scripts/ascii.js
@@ -0,0 +1,55 @@
+
+var aniIndex = 0;
+var timer;
+var aniValue;
+
+function load(){
+    starBtn     = document.getElementById("start");
+    stopBtn     = document.getElementById("stop");
+    animation   = document.getElementById("animation");
+    textArea    = document.getElementById("text-area");
+    fontSize    = document.getElementById("fontsize");
+    turbo       = document.getElementById("turbo");
+
+    starBtn.onclick     = start;
+    stopBtn.onclick     = stop;
+    fontSize.onchange   = sizeChange;
+    turbo.onchange      = speedChange;
+}
+function start(){
+    starBtn.disabled = true;
+    stopBtn.disabled = false;
+    animation.disabled = true;
+
+    curText = textArea.value;
+    timer = setInterval(intervalTimeout,250);
+    aniValue = ANIMATIONS[animation.value].split("=====\n");
+    aniIndex = 0;
+}
+
+function stop(){
+    starBtn.disabled = false;
+    stopBtn.disabled = true;
+    animation.disabled = false;
+    clearInterval(timer);
+    timer = null;
+    textArea.value = curText;
+}
+
+function sizeChange(){
+    textArea.style.fontSize = fontSize.value;
+}
+
+function speedChange(){
+    if(timer == null)   return;
+    var interval = turbo.checked==true?50:250;
+    clearInterval(timer);
+    timer = setInterval(intervalTimeout,interval);
+}
+
+function intervalTimeout(){
+    textArea.value = aniValue[aniIndex++];
+    if(aniIndex == aniValue.length) aniIndex = 0;
+}
+
+window.onload = load;
