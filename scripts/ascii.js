21bd28c3dba439acd650063994760b7d3a1dc626
Hang <hangmt03bk@yahoo.com>
2023-01-22 14:41:28 -0600

    [Hang] 01/21/2023 assignment5: animation

--- scripts/ascii.js
+++ scripts/ascii.js
@@ -1,10 +1,11 @@
 
-"use strict";
-var aniValue =[],aniIndex = 0;
-var timer;
-var starBtn, stopBtn, animation, textArea, fontSize, turbo, curText;
 
 function load(){
+    "use strict";
+    var aniValue =[],aniIndex = 0;
+    var timer;
+    var starBtn, stopBtn, animation, textArea, fontSize, turbo, curText;
+
     starBtn     = document.getElementById("start");
     stopBtn     = document.getElementById("stop");
     animation   = document.getElementById("animation");
@@ -16,42 +17,43 @@ function load(){
     stopBtn.onclick     = stop;
     fontSize.onchange   = sizeChange;
     turbo.onchange      = speedChange;
-}
-function start(){
-    starBtn.disabled = true;
-    stopBtn.disabled = false;
-    animation.disabled = true;
-
-    curText = textArea.value;
-    timer = setInterval(intervalTimeout,250);
-    aniValue = ANIMATIONS[animation.value].split("=====\n");
-    aniIndex = 0;
-}
-
-function stop(){
-    starBtn.disabled = false;
-    stopBtn.disabled = true;
-    animation.disabled = false;
-    clearInterval(timer);
-    timer = null;
-    textArea.value = curText;
-}
-
-function sizeChange(){
-    textArea.style.fontSize = fontSize.value;
-}
-
-function speedChange(){
-    if(timer == null)   return;
-    var interval = turbo.checked==true?50:250;
-    clearInterval(timer);
-    timer = setInterval(intervalTimeout,interval);
-}
-
-function intervalTimeout(){
-    if(aniValue.length == 0)    return;
-    textArea.value = aniValue[aniIndex++];
-    if(aniIndex == aniValue.length) aniIndex = 0;
+    function start(){
+        starBtn.disabled = true;
+        stopBtn.disabled = false;
+        animation.disabled = true;
+    
+        curText = textArea.value;
+        timer = setInterval(intervalTimeout,250);
+        aniValue = ANIMATIONS[animation.value].split("=====\n");
+        aniIndex = 0;
+    }
+    
+    function stop(){
+        starBtn.disabled = false;
+        stopBtn.disabled = true;
+        animation.disabled = false;
+        clearInterval(timer);
+        timer = null;
+        textArea.value = curText;
+    }
+    
+    function sizeChange(){
+        textArea.style.fontSize = fontSize.value;
+    }
+    
+    function speedChange(){
+        if(timer == null)   return;
+        var interval = turbo.checked==true?50:250;
+        clearInterval(timer);
+        timer = setInterval(intervalTimeout,interval);
+    }
+    
+    function intervalTimeout(){
+        if(aniValue.length == 0)    return;
+        textArea.value = aniValue[aniIndex++];
+        if(aniIndex == aniValue.length) aniIndex = 0;
+    }
+    
 }
 
 window.onload = load;
